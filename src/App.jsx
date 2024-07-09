f675a621424366d69731a86cf308e978f7dc26a1
Pankaj Rana <150144841+pankajrana754@users.noreply.github.com>
2024-07-10 01:04:09 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Card from './component/Card/Card'
+import Grid from './component/Grid/Grid'
+
+function App() {
+  return (
+    <>
+        <Grid numberOfCards={9}/>
+    </>
+  )
+}
+
+export default App
